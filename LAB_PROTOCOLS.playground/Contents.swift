d1198e20d5d3c02fe67081865793ec07a17c5c4a
dalal92 <dalal@MacBook-Pro.local>
2021-10-14 10:25:28 +0300

    Done^^

---
+++ LAB_PROTOCOLS.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+import Darwin
+
+protocol Bird {
+    
+    var color : String { get }
+    var wingspan : Int { get }
+    var canFly : Bool { get }
+    var canSwim : Bool { get }
+    
+    func fly()
+}
+    
+struct Duck : Bird {
+    
+    var color: String
+    var wingspan: Int
+    var canFly: Bool
+    var canSwim: Bool
+    
+    func fly() {
+        
+        if (canFly==true) {
+            print("This Duck is flying!")
+        }
+            else {
+                print("This Duck Cannot Fly")
+                
+            }
+    }}
+    
+
+let myDuck = Duck(color: "Black", wingspan: 24, canFly:true , canSwim: true)
+
+myDuck.fly()
+   
+    
+    
+    
+    
