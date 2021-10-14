b82aa5a266564344419450e95a1eae3a32b8f1da
Monafh <monafh@MacBook-Pro-alkhas-b-Monafh.local>
2021-10-14 11:01:38 +0300

    with using all information

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,25 +5,34 @@ protocol birds {
     var color : String{ get}
     var wingspan : Int{get}
     var canFly : Bool{get}
-    var canSwim : Bool{get }
-    func fly()
+    var canSwim : Bool{get}
+//    func fly()
+//    func myDuckInfo()
 }
 
 struct Duck : birds {
-    var color: String
-    var wingspan: Int
-    var canSwim: Bool
-    var canFly: Bool
-  
+    var color: String ,wingspan: Int ,canSwim: Bool ,canFly: Bool
+
     func fly() {
-   if (canFly == true){  //"flying"
-        print ("This Duck is flying!")
+   if (canFly == true){  //"flying" i can do if (canFly) only
+        print ("* This Duck is flying!")
     }
     else{
-            
-    print("This Duck Cannot Fly")
+
+    print("* This Duck Cannot Fly")
         }}
+    
+    func myDuckInfo(){
+        print("* My duck is \(color) and , it`s wing flapping \(wingspan)")
+    }
+    
+    
+    
 }
     let myduck = Duck(color: "white", wingspan: 7, canSwim: true, canFly: false)
+    let yourduck = Duck(color: "red", wingspan: 8, canSwim: false, canFly: true)
     myduck.fly()
-
+    yourduck.fly()
+print("〰️")
+myduck.myDuckInfo()
+yourduck.myDuckInfo()
