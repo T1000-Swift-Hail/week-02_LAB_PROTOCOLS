41055929c35a9bda93184d8a7d59772c51a86ec2
AsmaRashed1 <Asma.Rashed.has.s@tuwaiq.edu.sa>
2021-10-14 10:57:03 +0300

    Done lab

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+protocol Bird {
+    var  color : String { get }
+    var  wingspan : Int { get }
+    var  canFly : Bool { get }
+    var  canSwim : Bool { get}
+    
+    func fly()
+}
+
+struct Duck : Bird {
+ var color: String
+ var wingspan: Int
+ var canFly: Bool
+ var canSwim: Bool
+    
+ func fly()  {
+ if canFly {
+print("This Duck is flying!") }
+ else {
+  print("This Duck Cannot Fly") }
+ } }
+let duck1fly : Duck = Duck(color: "white", wingspan: 2, canFly: false, canSwim: true)
+
+duck1fly.fly()
+    
+    
+   
+
