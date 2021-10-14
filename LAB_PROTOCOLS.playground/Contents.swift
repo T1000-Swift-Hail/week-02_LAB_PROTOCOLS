7e839e1b77bd8901b6afbd8f91fb94f3a13000ce
Seham الشطنان <sehamalshtnan@Sehams-MacBook-Air.local>
2021-10-14 11:17:51 +0300

    done lap protocols

---
+++ LAB_PROTOCOLS.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+protocol Bird {
+    var Color : String { get }
+    var Wingspan : Int{get}
+    var CanFly : Bool {get}
+    var CanSwim : Bool{get}
+  func fly()
+}
+
+
+    
+    
+    
+struct Duck : Bird{
+    var Color: String
+    
+    var Wingspan: Int
+    
+    var CanFly: Bool
+    
+    var CanSwim: Bool
+    
+    func fly() {
+        if CanFly {
+        print("can Fly ")
+        } else {
+            print("This Duck Cannot Fly")
+        }
+    }
+}
+
+let pigeon = Duck(Color: "red", Wingspan: 2, CanFly: true, CanSwim: false)
+pigeon.fly()
+
+
+
+
+
+
+
+
+
+
+
