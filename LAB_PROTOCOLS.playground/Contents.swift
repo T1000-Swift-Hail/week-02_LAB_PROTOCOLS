6a42b9e29d0aa13110c75e8a0f0d6395fb84f9d1
Munira abdullah <muniraabdullah@MacBook-alkhas-b-Munira.local>
2021-10-14 10:55:30 +0300

    Done lab protocols

---
+++ LAB_PROTOCOLS.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+protocol Bird {
+    var color : String { get }
+    var wingspan : Int { get }
+    var canFly : Bool { get }
+    var canSwim : Bool { get }
+    
+    func fly()
+
+}
+struct Duck : Bird {
+    var color: String
+    
+    var wingspan: Int
+    
+    var canFly: Bool
+    
+    var canSwim: Bool
+    
+    func fly() {
+        if canFly {
+            print("This, Duck is flying!")
+        } else  {
+            print("This Duck Cannot Fly")
+        }
+    }
+}
+let Swan = Duck (color: "black" , wingspan: 12 , canFly: true , canSwim: true)
+
+Swan.fly()
