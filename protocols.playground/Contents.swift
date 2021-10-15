1424e384394f5671b532e6d5d95e418575a3ec14
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-10-15 17:12:44 +0300

    Done Lab

--- protocols.playground/Contents.swift
+++ protocols.playground/Contents.swift
@@ -1,3 +1,31 @@
 import UIKit
 
-var greeting = "Hello, playground"
+protocol Bird {
+    var Color : String {get}
+    var Wingspan : Int{get}
+    var CanFly : Bool{get}
+    var CanSwim : Bool{get}
+    func fly ()
+}
+
+
+
+
+struct Duck : Bird{
+var Color: String
+    var Wingspan: Int
+    var CanFly: Bool
+    var CanSwim: Bool
+    func fly() {
+        if CanFly {
+        print("Can Fly")
+        } else {
+    print("This Duck Cannot Fly")
+        }
+    }
+}
+let pigeon = Duck(Color:"red",Wingspan: 2,CanFly: true,CanSwim: false)
+pigeon.fly()
+
+
+
